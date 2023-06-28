1a0808cab360f5b7d256f2d063ed129fa27e472e
Muskan Singla <76451999+MuskanSingla18@users.noreply.github.com>
2023-06-28 15:39:27 +0530

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -93,21 +93,21 @@ function Footer({spotify}) {
 
             <div className="footer__center">
             <ShuffleIcon className="footer__green" />
-            <SkipPreviousIcon onClick={skipNext} className="footer__icon" />
+            <SkipPreviousIcon /*onClick={skipNext}*/ className="footer__icon" />
             {playing ? (
           <PauseCircleOutlineIcon
-            onClick={handlePlayPause}
+            /*onClick={handlePlayPause}*/
             fontSize="large"
             className="footer__icon"
           />
         ) : (
           <PlayCircleOutlineIcon
-            onClick={handlePlayPause}
+            /*onClick={handlePlayPause}*/
             fontSize="large"
             className="footer__icon"
           />
         )}
-           <SkipNextIcon onClick={skipPrevious} className="footer__icon" />
+           <SkipNextIcon /*onClick={skipPrevious}*/ className="footer__icon" />
             <RepeatIcon className="footer__green" />
             </div>
             <div className="footer__right">
